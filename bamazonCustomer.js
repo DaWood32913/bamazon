d1715bf495099eba98ecbdcb300c0435216a50c6
DaWood32913 <woodleyjohnson@yahoo.com>
2019-07-16 19:10:01 -0400

    initialized user selections

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,11 +28,45 @@ connection.connect(function (err) {
 // Initializing Shopping
 startShopping();
 
+//Displays Store Item Database in CLI Table
 function startShopping() {
     connection.query('SELECT * FROM Products', function (err, res) {
         for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price.toFixed(2), res[i].stock_quantity])
         }
         console.log(table.toString());
+
+//Prompts User to select an item to purchase        
+        inquirer.prompt([{
+            name: "choice",
+            type: "list",
+            message: "Select an item to purchase.",
+
+            choices: function (value) {
+                var choiceArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    choiceArray.push(res[i].product_name);
+                }
+                return choiceArray;
+            }
+          }, {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                  return false;  
+                }
+            }
+            
+        }]).then(function (answer) {
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].product_name === answer.choice) {
+                    var selectedItem = res[i];
+                }
+            }
+        })
     })
 }
